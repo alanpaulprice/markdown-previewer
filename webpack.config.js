40362c1957ebba575cc8001921fdc221deb45787
alanpaulprice <23743415+alanpaulprice@users.noreply.github.com>
2018-03-01 13:23:21 +0000

    completed initial config and installation of packages

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname)
+  },
+  devServer: {
+    contentBase: './',
+    historyApiFallback: true,
+    port: 1234
+  },
+  module: {
+  rules: [
+    {
+      test: /\.js$/,
+      exclude: /node_modules/,
+      loader: "babel-loader"
+    }
+  ]
+},
+  mode: 'development'
+};
