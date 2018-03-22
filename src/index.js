2786432ae1932ba255e418c77b31c5cebd92ffb2
alanpaulprice <23743415+alanpaulprice@users.noreply.github.com>
2018-03-22 17:38:37 +0000

    fixed textarea resizing issue on smaller screens

--- src/index.js
+++ src/index.js
@@ -1,8 +1,3 @@
-// example: https://codepen.io/freeCodeCamp/full/JXrLLE
-// https://github.com/alanpaulprice/markdown-previewer
-//TODO: fix issue where output doesn't come up when resizing input smaller
-// on mobile
-
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import marked from 'marked';
